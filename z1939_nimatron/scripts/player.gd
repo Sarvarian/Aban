49e0a7d6cdc46a8e95bde811354fd0c85aced206
Sarvarian <Sarvarian@Hotmail.com>
2020-10-31 10:27:36 -0200

    submit button

--- z1939_nimatron/scripts/player.gd
+++ z1939_nimatron/scripts/player.gd
@@ -12,37 +12,37 @@ var do_move : bool = false
 
 
 func submit() -> void:
-    if my_turn and do_move:
-        emit_signal("submit")
-        my_turn = false
+	if my_turn and do_move:
+		emit_signal("submit")
+		my_turn = false
 #
 
 func line0() -> void:
-    emit_signal("turn_off", 0)
-    do_move = true
-    emit_signal("go_green")
+	emit_signal("turn_off", 0)
+	do_move = true
+	emit_signal("go_green")
 #
 func line1() -> void:
-    emit_signal("turn_off", 1)
-    do_move = true
-    emit_signal("go_green")
+	emit_signal("turn_off", 1)
+	do_move = true
+	emit_signal("go_green")
 #
 func line2() -> void:
-    emit_signal("turn_off", 2)
-    do_move = true
-    emit_signal("go_green")
+	emit_signal("turn_off", 2)
+	do_move = true
+	emit_signal("go_green")
 #
 func line3() -> void:
-    emit_signal("turn_off", 3)
-    do_move = true
-    emit_signal("go_green")
+	emit_signal("turn_off", 3)
+	do_move = true
+	emit_signal("go_green")
 #
 
 func line_reset() -> void:
-    do_move = false
-    emit_signal("go_red")
+	do_move = false
+	emit_signal("go_red")
 #
 
 func player_turn() -> void:
-    my_turn = true
+	my_turn = true
 #
--- z1939_nimatron/scripts/submit_button.gd
+++ z1939_nimatron/scripts/submit_button.gd
@@ -1,1 +1,17 @@
-extends TouchScreenButton
+extends Node2D
+
+
+func go_red() -> void:
+	$AnimationPlayer.play("Red")
+#
+
+func go_green() -> void:
+    $AnimationPlayer.play("Green")
+#
+
+func pressed() -> void:
+    modulate.v = .5
+#
+
+func released() -> void:
+    modulate.v = 1
