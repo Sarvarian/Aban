d5598a4d2ef7ae301a02a157b16e3fd3de5daa63
Sarvarian <Sarvarian@Hotmail.com>
2020-11-01 06:52:35 -0200

    fix 2 bugs of reset green light and multi line

--- z1939_nimatron/scripts/game.gd
+++ z1939_nimatron/scripts/game.gd
@@ -7,7 +7,6 @@ signal line_reset()
 
 
 var is_player_turn : bool = true
-var last_line_that_get_turn_off : Node2D = null
 
 
 func _ready() -> void:
@@ -39,3 +38,6 @@ func submit() -> void:
 func line_reset() -> void:
 	emit_signal("line_reset")
 
+
+func reset(line_index : int) -> void:
+	get_child(line_index).reset()
--- z1939_nimatron/scripts/player.gd
+++ z1939_nimatron/scripts/player.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 signal turn_off(line_index)
+signal reset(line_index)
 signal submit()
 signal go_red()
 signal go_green()
@@ -9,6 +10,7 @@ signal go_green()
 
 var my_turn : bool = true
 var do_move : bool = false
+var last_line_index : int = -1
 
 
 func submit() -> void:
@@ -18,9 +20,17 @@ func submit() -> void:
 #
 
 func line_pressed(line_index : int) -> void:
-	emit_signal("turn_off", line_index)
+	if not my_turn:
+		return
+	if line_index != last_line_index:
+		if last_line_index < 0:
+			print(last_line_index)
+			last_line_index = line_index
+		emit_signal("reset", last_line_index)
+	last_line_index = line_index
 	do_move = true
 	emit_signal("go_green")
+	emit_signal("turn_off", line_index)
 
 
 func line_reset() -> void:
